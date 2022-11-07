13c2de6657b134b5d7ba8aba91d5bfefc429afb3
Henrique Oliveira <henrique.oliveira0101@gmail.com>
2022-11-07 15:51:39 -0300

    funções no imc.JS

--- imc.js
+++ imc.js
@@ -8,8 +8,28 @@ function imc () {
     const resultado   = document.getElementById('resultado');
 
     // validação dos campos (preenchimento)
-    if (nome !== '') {
-        alert ('valor preenchido')
+    if (nome !== '' && altura !== '' && peso !== '') {
+       
+        const valorIMC = (peso / (altura*altura)).toFixed(2);
+        let classificacao = '';
+            if (valorIMC < 18.5) {
+                classificacao = 'abaixo do peso';
+            }else if (valorIMC < 25){
+                classificacao = 'com peso ideal. Parabéns!!!';           
+            }else if (valorIMC < 30){
+                classificacao = 'levemente acima do peso.';
+            }else if (valorIMC < 35){
+                classificacao = 'com obesidade grau I.';
+            }else if (valorIMC < 40){
+                classificacao = 'com obesidade grau II.';
+            }else {classificacao = 'com obesidade grau III. Cuidado!';
+            }
+
+
+        resultado.textContent = ` ${nome} seu IMC é ${valorIMC} e você está ${classificacao} `;
+
+    } else{
+        resultado.textContent = 'Preenchar todos os campos'
     }
 
 }
