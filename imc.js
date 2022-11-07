88e64915858fab5f07d29ae76a1e2d6431f9f8b5
Henrique Oliveira <henrique.oliveira0101@gmail.com>
2022-11-07 15:16:05 -0300

    calculado IMC

---
+++ imc.js
@@ -0,0 +1,17 @@
+
+const calcular   = document.getElementById('calcular');
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso   = document.getElementById('peso').value;
+    const resultado   = document.getElementById('resultado');
+
+    // validação dos campos (preenchimento)
+    if (nome !== '') {
+        alert ('valor preenchido')
+    }
+
+}
+
+calcular.addEventListener('click', imc);
